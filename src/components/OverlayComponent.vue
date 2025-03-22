f611fc0f95e0c3e5507bab899c9e83b3b04f6934
suhyunpark <psh7492@gmail.com>
2025-03-22 13:49:10 +0900

    fix: overlaycomponent, sidebarcomponent로 분리

---
+++ src/components/OverlayComponent.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="overlay" @click="goToDecoPage">
+        <!-- 홈버튼 클릭 시 deco 페이지로 이동 -->
+        <button class="floating-btn" @click="goToDecoPage">
+            <v-icon icon="mdi-home-outline" style="font-size: 60px;" />
+        </button>
+
+        <!-- 본문 내용 -->
+        <v-card class="content" @click.stop>
+            <v-row>
+                <!-- 사이드바 -->
+                <v-col cols="3">
+                    <slot name="sidebar"></slot>
+                </v-col>
+
+                <!-- 콘텐츠 부분 -->
+                <v-col cols="9">
+                    <slot name="content"></slot>
+                </v-col>
+            </v-row>
+        </v-card>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    components: {
+
+    },
+    data() {
+        return {
+        };
+    },
+    methods: {
+        goToDecoPage() {
+            this.$router.push('/deco');
+        },
+    },
+};
+</script>
+
+<style scoped>
+.overlay {
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(10px);
+    /* 모달처럼 흐림 효과 */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.content {
+    background-color: beige;
+    width: 80%;
+    height: 80%;
+    padding: 20px;
+    border-radius: 20px;
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.2);
+}
+
+.floating-btn {
+    color: #000;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    background: white;
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ src/components/SidebarComponent.vue
@@ -0,0 +1,83 @@
+<template>
+    <!-- 프로필 -->
+    <v-card class="profile-card" variant="flat">
+        <img src="../img/Chihuahua.jpg" width="100%" alt="" class="profile-img">
+        <v-card-title>이거 사이드바임</v-card-title>
+    </v-card>
+
+    <!-- 알림 -->
+    <v-card class="notification-card" variant="flat">
+        <v-card-title>
+            <v-icon icon="mdi-bell-outline" style="font-size: 24px;" />
+            새로운 알림
+        </v-card-title>
+        <v-list class="notifications">
+            <v-list-item v-for="(item, index) in notifications" :key="index">
+                <v-list-item-title>{{ item }}</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            notifications: [
+                "아옹님이 새 친구가 되었습니다",
+                "아옹님이 나의 댓글에 좋아요를 눌렀습니다",
+                "아옹님이 5월 7일 일기에 댓글을 남겼습니다",
+                "스니커즈님이 새로운 일기를 작성하였습니다.",
+                "스니커즈님이 나의 댓글에 좋아요를 눌렀습니다."
+            ],
+        };
+    },
+};
+</script>
+
+<style scoped>
+.content {
+    margin: 0;
+    border-radius: 20px;
+}
+
+.profile-card {
+    text-align: center;
+    border-radius: 0;
+}
+
+.profile-img {
+    padding: 10px;
+    border-radius: 10%;
+}
+
+.notification-card {
+
+    border-radius: 0;
+}
+
+.calendar-card {
+    padding: 16px;
+    border-radius: 0;
+    height: 90%;
+}
+
+.tab-content {
+    padding: 20px;
+    margin: 0 2px;
+    padding: 12px 20px;
+    background-color: red;
+}
+
+:deep(.v-tab) {
+    /* v-tab에 border-radius 적용 */
+    border-radius: 12px 12px 0 0 !important;
+    overflow: hidden;
+}
+
+.v-tab--selected {}
+
+* {
+    color: black;
+}
+</style>
