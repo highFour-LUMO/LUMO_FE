3e12b2aa8ba9a9c54d5e594d4a9a94cd1aaa1a28
suhyunpark <psh7492@gmail.com>
2025-03-22 13:53:44 +0900

    feat: decopage 버튼으로 라우팅 설정

--- src/views/DecoPage.vue
+++ src/views/DecoPage.vue
@@ -1,32 +1,24 @@
 <template>
     <div class="deco-container">
-        <btn @click.stop="dialog = true">
+        <button class="floating-btn" @click="$router.push('/home')">
             <v-icon icon="mdi-home-outline" style="font-size: 60px;" />
-        </btn>
-        <HomePage :value="dialog" @input="dialog = $event"></HomePage>
+        </button>
     </div>
 </template>
 
-<script>
-import HomePage from './HomePage.vue';
-
-export default {
-    data() {
-        return {
-            dialog: false,
-        };
-    },
-    components: {
-        HomePage,
-    },
-};
-</script>
-
 <style scoped>
-.deco-container {
-    /*임시로 이미지 정했는데 변경 예정입니다*/
-    background-image: url('../img/nightsky.jpg');
-    background-size: cover;
-    height: 100vh;
+.floating-btn {
+    color: black;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    background: white;
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
 }
 </style>
